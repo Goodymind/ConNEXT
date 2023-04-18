745a09a93977e5d5465b3dcf39db7612bb00c6f5
Alinus Abuke <aabuke9@gmail.com>
2023-04-18 10:02:25 +0800

    Extra commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#ConNEXT
+## an automatic flowchart generator for python codes
+>It basically creates flowcharts, that's it.
+A research product of students from Valenzuela City School of Mathematics and Science.
+**Proponents**: Alinus Abuke, Alessia Aguilar, Chriciella Buenanventura, Jazmin Althea Santos, Charlene Sentillas
+>Special Thanks To Doc Lo (our favorite teacher) for being a __very__ supportive individual!
---
+++ flowcharter/test.py
@@ -0,0 +1,24 @@
+def FunctionOne():
+    print('function one')
+    print('function one')
+    print('function one')
+    print('function one')
+    print('function one')
+    if True:
+        print('True')
+        print('True')
+        print('True')
+        print('True')
+        print('True')
+    elif True:
+        print('elif')
+        print('elif')
+        print('elif')
+        print('elif')
+        print('elif')
+    else:
+        print('Else')
+        print('Else')
+        print('Else')
+        print('Else')
+    print('Then')
